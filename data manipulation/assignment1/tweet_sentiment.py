877fbaa9e7e8ecc9ef66351bdee3d1133f0b0392
Leo Romanovsky <leoromanovsky@gmail.com>
2015-11-03 01:34:37 -0800

    Determine tweet sentiment.

--- data manipulation/assignment1/tweet_sentiment.py
+++ data manipulation/assignment1/tweet_sentiment.py
@@ -1,17 +1,27 @@
 import sys
-
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
+import json
 
 def main():
     sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+
+    scores = {} # initialize an empty dictionary
+    for line in sent_file:
+        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term] = int(score)  # Convert the score to an integer.
+
+    with open(sys.argv[2]) as f:
+        content = f.read().splitlines()
+        for row in content:
+            data = json.loads(row)
+            if 'text' in data:
+                text = data['text']
+                words = text.split(' ')
+                tweet_score = 0
+                for word in words:
+                    if word in scores:
+                        tweet_score += scores[word]
+
+                print tweet_score
 
 if __name__ == '__main__':
     main()
